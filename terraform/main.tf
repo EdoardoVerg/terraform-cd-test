1f33d54430526609e8e50edbec1ae673aaaa9533
Edoardo Vergani <edoardo.vergani@soft.it>
2025-03-05 09:50:24 +0100

    Removed some files

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,28 @@ provider "google" {
   region  = var.region
 }
 
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  default     = "europe-west3"
+  type        = string
+}
+
+variable "service_name" {
+  description = "Cloud Run service name"
+  default     = "my-cloud-run-service"
+  type        = string
+}
+
+variable "service_account_email" {
+  description = "Service account email for Cloud Run"
+  type        = string
+}
+
 resource "google_cloud_run_service" "default" {
   name     = var.service_name
   location = var.region
