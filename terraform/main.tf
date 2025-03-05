ca4fde27da36ded2a9a8f7faa1a8028248adc4a4
Edoardo Vergani <edoardo.vergani@soft.it>
2025-03-05 09:47:57 +0100

    Edited .yml and .tf files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
 resource "google_cloud_run_service" "default" {
   name     = var.service_name
   location = var.region
-
-  # Add this lifecycle block
-  lifecycle {
-    ignore_changes = [
-      template[0].metadata[0].annotations,
-      metadata[0].annotations
-    ]
-  }
 
   template {
     spec {
@@ -23,4 +32,36 @@ resource "google_cloud_run_service" "default" {
     latest_revision = true
     percent         = 100
   }
+
+  # Force recreation of the service
+  lifecycle {
+    replace_triggered_by = [
+      null_resource.always_run.id
+    ]
+  }
+}
+
+# Force recreation trigger
+resource "null_resource" "always_run" {
+  triggers = {
+    timestamp = timestamp()
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "public" {
+  location = google_cloud_run_service.default.location
+  service  = google_cloud_run_service.default.name
+
+  policy_data = jsonencode({
+    bindings = [
+      {
+        role = "roles/run.invoker"
+        members = ["allUsers"]
+      }
+    ]
+  })
+}
+
+output "service_url" {
+  value = google_cloud_run_service.default.status[0].url
 }
